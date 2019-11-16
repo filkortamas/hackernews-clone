bafaafde790c116012748fa5c0239a10a7260498
Filkor Tamás <filkordev@protonmail.com>
2019-11-16 21:11:34 +0100

    theme context

---
+++ app/components/Home.js
@@ -0,0 +1,65 @@
+import React from 'react';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  NavLink
+} from 'react-router-dom';
+
+import Stories from './Stories';
+import User from './User';
+import Comments from './Comments';
+import { ThemeContext } from '../context/theme';
+
+export default function Home() {
+  return (
+    <ThemeContext.Consumer>
+      {({ theme, toggleTheme }) => (
+        <div className={theme}>
+          <div className="container">
+            <Router>
+              <nav className="space-between">
+                <ul className="nav-bar">
+                  <li className="nav-item">
+                    <NavLink
+                      className="nav-link"
+                      activeClassName="active"
+                      exact
+                      to="/"
+                    >
+                      Top
+                    </NavLink>
+                  </li>
+                  <li className="nav-item">
+                    <NavLink className="nav-link" exact to="/new">
+                      New
+                    </NavLink>
+                  </li>
+                </ul>
+                <button className="btn-clear" onClick={() => toggleTheme()}>
+                  {theme === 'light' ? '🔦' : '💡'}
+                </button>
+              </nav>
+              <Switch>
+                <Route
+                  key="/"
+                  exact
+                  path="/"
+                  render={props => <Stories {...props} />}
+                />
+                <Route
+                  key="/new"
+                  exact
+                  path="/new"
+                  render={props => <Stories category="new" {...props} />}
+                />
+                <Route path="/user" component={User} />
+                <Route path="/post" component={Comments} />
+              </Switch>
+            </Router>
+          </div>
+        </div>
+      )}
+    </ThemeContext.Consumer>
+  );
+}
